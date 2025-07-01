a0b9bc7961cd2b512415daae40267b42e59c236a
UnesABA <unesaitbenali@gmail.com>
2025-07-01 19:11:58 +0100

    Add review creation logic and link reviews to corresponding tours

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -67,7 +67,7 @@ export const getSingleTour = async (req, res) => {
   const id = req.params.id
 
   try {
-    const tour = await Tour.findById(id)
+    const tour = await Tour.findById(id).populate("reviews")
 
     return res.status(200).json({
       success: true,
@@ -89,6 +89,7 @@ export const getAllTours = async (req, res) => {
 
   try {
     const tours = await Tour.find({})
+      .populate("reviews")
       .skip(page * 8)
       .limit(8)
 
@@ -120,7 +121,7 @@ export const getTourBySearch = async (req, res) => {
       city,
       distance: { $gte: distance },
       maxGroupSize: { $gte: maxGroupSize },
-    })
+    }).populate("reviews")
     return res.status(200).json({
       success: true,
       message: "Matching tours retrieved successfully.",
@@ -140,7 +141,9 @@ export const getFeaturedTours = async (req, res) => {
   try {
     const featuredTours = await Tour.find({
       featured: true,
-    }).limit(8)
+    })
+      .populate("reviews")
+      .limit(8)
     return res.status(200).json({
       success: true,
       count: featuredTours.length,
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser"
 import tourRoute    from "./routes/tours.js"
 import userRoute    from "./routes/users.js"
 import authRoute    from "./routes/Auth.js"
+import reviewRoute  from "./routes/reviews.js"
 
 const corsOptions = {
   origin: true,
@@ -35,6 +36,7 @@ app.use(cookieParser())
 app.use("/api/v1/tours", tourRoute)
 app.use("/api/v1/users", userRoute)
 app.use("/api/v1/auth", authRoute)
+app.use("/api/v1/review", reviewRoute)
 
 app.listen(port, () =>{
   connect()
