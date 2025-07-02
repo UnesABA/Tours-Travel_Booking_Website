33bada7935e5f7f8ccb3c7796e5a7e090e4a5cb6
UnesABA <unesaitbenali@gmail.com>
2025-07-02 17:57:00 +0100

    Add custom useFetch hook for data fetching with loading and error handling

---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+
+const useFetch = (url) => {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() =>{
+    const fetchData = async ()=>{
+      setLoading(true)
+      try {
+        const res = await fetch(url)
+        if(!res.ok){
+          setError("Failed to fetch")
+          alert("Failed to fetch")
+        }
+
+        const resultat = res.json()
+        setData(resultat.data)
+        
+      } catch (error) {
+        setError(error.message)
+        alert("Failed to fetch")
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [url])
+  return {
+    data,
+    error,
+    loading
+  }
+}
+
+export default useFetch
